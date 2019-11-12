2fb72af4ae439c5486e1979684e8f7f9e5a006f8
2B Modular Info <2binfo@basilides.local>
2019-11-12 20:36:12 -0300

    Upando projeto

---
+++ AppMySqlMVC/AppMySqlMvDAO/ConexaoDB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace AppMySqlMvDAO
+{
+    class ConexaoDB
+    {
+        private static ConexaoDB objConexaoDB = null;
+        private readonly MySqlConnection Conexao;
+
+        private ConexaoDB()
+        {
+            Conexao = new MySqlConnection("server=localhost; user id=root; password=1234567; database=dbECOMMERCE");
+        }
+
+        public static ConexaoDB ConexaoStatus()
+        {
+            if(objConexaoDB == null)
+            {
+                objConexaoDB = new ConexaoDB();
+            }
+            return objConexaoDB;
+        }
+
+        public MySqlConnection ConexaoPegar()
+        {
+            return Conexao;
+        }
+
+        public void ConexaoFechar()
+        {
+            objConexaoDB = null;
+        }
+    }
+}
